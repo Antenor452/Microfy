e51fc3bbd654c62fd2c7410604625f4ff754031e
Antenor <nanaosei452@gmail.com>
2022-08-03 20:36:13 +0000

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import HomePage from "./Pages/HomePage";
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  useEffect(() => {
+    const logInStatus = JSON.parse(localStorage.getItem("logInStatus"));
+    if (logInStatus) {
+      setIsSignedIn(true);
+    }
+    setIsSignedIn(false);
+    console.log(logInStatus, isSignedIn);
+  }, []);
   return (
     <React.Fragment>
       <Header />
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage isSignedIn={isSignedIn} />} />
         </Routes>
       </Router>
     </React.Fragment>
---
+++ src/HelperFiles/firebaseSetup.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGe5FtJ_83iYuLhVTRsRPz_weY20smHxc",
+  authDomain: "microfy-f49a1.firebaseapp.com",
+  projectId: "microfy-f49a1",
+  storageBucket: "microfy-f49a1.appspot.com",
+  messagingSenderId: "452598260301",
+  appId: "1:452598260301:web:ea240bc33a910432d0f204",
+  measurementId: "G-XTMKJTJWHE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
