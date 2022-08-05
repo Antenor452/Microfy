c1adb7db08c5bbdb6682ecc64fc81c782d83960d
Antenor <nanaosei452@gmail.com>
2022-08-05 22:45:29 +0000

    added error screen and image

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,7 +1,42 @@
 import React from "react";
+import "../styling/errorstyle.css";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+  const backToHome = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <main>
+        <div className="container vh-100 d-flex align-items-center justify-content-center">
+          <h4 className="ms-lg-5 mt-lg-5">404 not found</h4>
+          <div className="row mt-lg-0 mt-4 ">
+            <div className="col-lg-6 d-flex justify-content-center align-items-center ">
+              <img
+                src={require("../Images/Scarecrow.png")}
+                className="img-fluid scare"
+              />
+            </div>
+
+            <div className="col-lg-6">
+              <h3>I have bad news for you</h3>
+              <div className="col-md-8">
+                <p>
+                  The page you are looking does not exist or might have been
+                  removed
+                </p>
+              </div>
+              <button className="btn mt-3 " onClick={backToHome}>
+                Back to homepage
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Error;
